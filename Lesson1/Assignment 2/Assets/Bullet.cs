b1ccfe59ce891f15904eb93f1541c33e6917f21e
Rungrukuk <Rungrukuk@gmail.com>
2023-04-20 01:28:04 +0400

    Still could not solve how to rotate 2d objects

---
+++ Lesson1/Assignment 2/Assets/Bullet.cs
@@ -0,0 +1,26 @@
+
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private LaserGun laserGun;
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float rayDistance = 50f;
+    void Start()
+    {
+        laserGun = GameObject.FindWithTag("Player").GetComponent<LaserGun>();
+        transform.rotation = Quaternion.LookRotation(Vector3.forward,transform.position -laserGun.transform.position);
+    }
+
+
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector3(0,0.01f * speed,0));
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.rotation * new Vector2(0, 7));
+        // if (hit.collider!=null)
+        // {
+        //     hit.
+        // }
+    }
+    
+}
--- Lesson1/Assignment 2/Assets/LaserGun.cs
+++ Lesson1/Assignment 2/Assets/LaserGun.cs
@@ -1,15 +1,33 @@
 using UnityEngine;
 
+
 public class LaserGun : MonoBehaviour
 {
     private Vector3 mousePosition;
     private float dotProduct;
     private float angle;
+    [HideInInspector]
+    public Vector3 shootingMousePos;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] Transform shootingPos;
+    [SerializeField] private GameObject bullets;
 
     // Update is called once per frame
     void Update()
     {
         if (Camera.main != null) mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.rotation = Quaternion.LookRotation(Vector3.forward,mousePosition - transform.position);
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = Quaternion.identity;
+            var position = transform.position;
+            dotProduct = position.normalized.x * mousePosition.x + position.normalized.y * mousePosition.y;
+            angle = Mathf.Acos(dotProduct / mousePosition.magnitude * position.normalized.magnitude) * 180 / Mathf.PI;
+            transform.rotation = Quaternion.Euler(0,0,angle);
+            shootingMousePos = mousePosition;
+            var bullet = Instantiate(bulletPrefab, shootingPos);
+            bullet.transform.parent = bullets.transform;
+        }
+        
     }
 }
